a0625adfea70c38644b98faf6a6d31403ea73ea9
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2021-09-27 13:55:19 +0100

    2020-2021

--- scripts/build-and-push.sh
+++ scripts/build-and-push.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 cd ../website && \
-#git init . && \
+#git init . &&\
 #git remote add origin git@github.com:ULL-MII-SYTWS-2122/ULL-MII-SYTWS-2122.github.io.git && \
 #touch .nojekyll && \
 git add . && \
 git ci -am 'new build' && \
 git push --force sytws main && \
-git push --force sytws2021 main
+git push --force sytws2122 main
